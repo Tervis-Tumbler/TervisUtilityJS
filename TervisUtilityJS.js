bcda8f74ab5c05a9247265e7a352b3616b95a6bf
Chris Magnuson <hourglasssand@gmail.com>
2019-05-08 13:31:54 -0400

    Initial commit

---
+++ TervisUtilityJS.js
@@ -0,0 +1,35 @@
+export function Add_MemberScriptProperty ({
+    $InputObject,
+    $Name,
+    $Value
+}) {
+    if (Array.isArray($InputObject)) {
+        for (var $InputObjectInstance of $InputObject) {
+            Object.defineProperty($InputObjectInstance, $Name, {
+                get: $Value
+            })
+        }
+    } else {
+        Object.defineProperty($InputObject, $Name, {
+            get: $Value
+        })
+    }
+} 
+
+export function New_HashTableIndex ({
+    $InputObject,
+    $PropertyToIndex
+}) {
+    var $HashTable = {}
+    for (const $InputObjectInstance of $InputObject) {
+        const $PropertyToIndexPropertyValue = $InputObjectInstance[$PropertyToIndex]
+        if (Array.isArray($PropertyToIndexPropertyValue)) {
+            for (const $PropertyToIndexValueInstance of $PropertyToIndexPropertyValue) {
+                $HashTable[$PropertyToIndexValueInstance] = $InputObjectInstance
+            }
+        } else {
+            $HashTable[$PropertyToIndexPropertyValue] = $InputObjectInstance
+        }
+    }
+    return $HashTable
+}
